679b5433192fe67b109c279bfa621550492ba3f8
ibrahimsugun <1150606905@nku.edu.tr>
2023-03-11 02:10:09 +0300

    googlesearch

--- assets/js/script.js
+++ assets/js/script.js
@@ -136,17 +136,6 @@ const initSlider = function (currentSlider) {
 for (let i = 0, len = sliders.length; i < len; i++) { initSlider(sliders[i]); }
 
 
- /*
-
-  
-
-setTimeout(function() {
-  window.location.href = "https://toplist29.tr.gg?in=368";
-}, 2000);
-setTimeout(function() {
-  window.location.href = "https://hitlist34.tr.gg?in=20";
-}, 2000);
-*/
 
 
 /* document.addEventListener('contextmenu', event => event.preventDefault());
